aa371b4f299be87a44c9408941fc7ba00572b59e
Will Lobb <155993877+willlobb@users.noreply.github.com>
2025-02-10 22:00:39 -0500

    removed index import

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'C:/Users/Will/OneDrive - University of Louisville/Desktop/lrnkna/my-react-app/src/index.css'
 import App from './App.jsx'
 import './App.css'
 
